7d8141526abfdb85efb4568dde43942bb84369c1
SunixDalsmith <152206156+SunixDalsmith@users.noreply.github.com>
2023-12-03 20:02:22 +0000

    PORTA COM COLISAO NA ANIMACAO
    EZ

---
+++ Jogo na Godot/Nodes/Porta/door.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+@onready var animation = $AnimationDoor #importar a animacao da porta
+var estate = "open" #fechar ou abrir a porta
+
+#
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_E: #se a tecla e for precionada
+			if estate == "open": #abre
+				animation.play("Open_Door")
+				estate = "close"
+			elif estate == "close": #fecha
+				animation.play("Close_Door")
+				estate = "open"
+
+
+
+func process(delta):
+	_input(0)
+
+
