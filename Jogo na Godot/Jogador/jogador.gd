c8c1365f7e4f0ab5d3ee05c33d97a5cf4398b9ad
SunixDalsmith <152206156+SunixDalsmith@users.noreply.github.com>
2023-12-03 10:45:36 +0000

    Jogo
    jogo

---
+++ Jogo na Godot/Jogador/jogador.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed: int = 50
+@onready var animation = $AnimationPlayer
+
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+	
+	
+func updateAnimation():
+	if velocity.length() == 0:
+		animation.stop()
+	else:
+		var direction = "_down"
+		if velocity.x < 0: direction = "_left"
+		elif velocity.x > 0: direction = "_right"
+		elif velocity.y < 0: direction = "_up"
+		elif velocity.y > 0: direction = "_down"
+		animation.play("walk" + direction)
+
+
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
